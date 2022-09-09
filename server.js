905e4a67a445b09aee4cc812734d2770770e1de4
Aditya Irri <adityairri@gmail.com>
2022-09-09 15:23:56 +0530

    port changed

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ app.get('/changeFarmerStatus/:id/:status', farmerResStatus);
 var installationStatus = require('./backbone/installationStatus');
 app.get('/viewInstallationStatus/:status', installationStatus);
 /****************** Run Server ***************************************/
-app.listen(8080);
+app.listen(80);
 console.log('Server is listening on port 8080');
 /****************** #END# Run Server *********************************/
